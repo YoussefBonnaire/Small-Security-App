ab6ff9e359d0ca4d45ddf75c75302d02f1a99a2c
McLellan, John <jm232@hw.ac.uk>
2021-11-29 16:34:05 +0000

    Update Verify.py

--- Verify.py
+++ Verify.py
@@ -5,6 +5,7 @@ from cryptography import x509
 signatureListFiles = 'app_files/signatures_list'
 x509CertListFiles = 'app_files/x509_cert_list'
 pgpCertListFiles = 'app_files/pgp_cert_list'
+plaintext_file = 'Plain_text_J&Y.txt'
 #Read in signatures and certificates
 signatureList = []
 with open(signatureListFiles,"r") as sigFiles:
@@ -27,9 +28,12 @@ with open(pgpCertListFiles,"r") as pgpCertFile:
         certificate = PGPKey.from_file(file)
         pgpCertList.append(certificate)
 
+with open(plaintext_file, "r") as plainfile:
+    plain_text = plainfile.read()
+
 # verify
 for i in range(len(signatureList)):
-    verifications = pgpCertList[i].verify(signatureList[i])
+    verifications = pgpCertList[i].verify(plain_text, signatureList[i])
     sigVerified = False
     for verSig in verifications.good_signatures:
         if verSig.verified:
